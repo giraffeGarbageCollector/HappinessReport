94d5c9cff684da29800bc581203cf756b7cc1f04
HengjunWU <hengjun.wu@uvm.edu>
2019-10-02 16:56:06 -0400

    validation for user input

--- query_str.py
+++ query_str.py
@@ -1,6 +1,5 @@
 def validate_query(query_str):
     qualifiers_lst = ['HAPPY', 'SUPPORTIVE', 'FREE', 'CORRUPT', 'GENEROUS', 'WEALTHY', 'HEALTHY']
-    noun_lst = ['COUNTRY', 'LANGUAGE', 'REGION']
     year_lst = ["2015", "2016", "2017", "2018", "2019"]
     rank_lst = ['TOP', 'Bottom', 'MOST', 'LEAST']
 
@@ -13,67 +12,125 @@ def validate_query(query_str):
             if splited_input[2] not in qualifiers_lst:
                 return False
             else:
-                if splited_input[3] not in noun_lst:
+                if splited_input[3] !="COUNTRY":
                     return False
                 else:
-                    ## length 4 , 5, 6
+                    # 2 most happy country
                     if len(splited_input) == 4:
-                        if splited_input[3] in noun_lst:
+                        if splited_input[3] == "COUNTRY":
                             return True
                         else:
                             return False
+                    # 2 most happy country 2015
                     elif len(splited_input) == 5:
-                        if splited_input[4] not in noun_lst:
-                            if splited_input[4] not in year_lst:
-                                return False
-                            else:
+                        if splited_input[4] != "COUNTRY":
+                            if splited_input[4] in year_lst:
                                 return True
-                        elif (splited_input[4] in noun_lst) and (splited_input[4]!=splited_input[3]):
-                            return True
+                            else:
+                                return False
                         else:
                             return False
+                    # 2 most happy country in region
+                    # 2 most happy country language speaking
                     elif len(splited_input) == 6:
-                        if splited_input[4] not in noun_lst:
-                            return False
-                        elif splited_input[4] == splited_input[3]:
-                            return False
-                        elif splited_input[5] not in year_lst:
+                        if splited_input[4] != "IN" and splited_input[4] != "LANGUAGE":
                             return False
                         else:
-                            return True
-    ## most happy country region 2015
+                            if splited_input[4] == "IN":
+                                if splited_input[5] == "REGION":
+                                    return True
+                                else:
+                                    return False
+                            elif splited_input[4] == "LANGUAGE":
+                                if splited_input[5] == "SPEAKING":
+                                    return True
+                                else:
+                                    return False
+                    # 2 most happy country in region 2015
+                    # 2 most happy country language speaking 2015
+                    elif len(splited_input) == 7:
+                        if splited_input[4] != "IN" and splited_input[4] != "LANGUAGE":
+                            return False
+                        else:
+                            if splited_input[4] == "IN":
+                                if splited_input[5] == "REGION":
+                                    if splited_input[6] in year_lst:
+                                        return True
+                                    else:
+                                        return False
+                                else:
+                                    return False
+                            elif splited_input[4] == "LANGUAGE":
+                                if splited_input[5] == "SPEAKING":
+                                    if splited_input[6] in year_lst:
+                                        return True
+                                    else:
+                                        return False
+                                else:
+                                    return False
+
+
+    ################################ most happy country region 2015 ############################
     elif splited_input[0] in rank_lst:
         if splited_input[1] not in qualifiers_lst:
             return False
         else:
-            if splited_input[2] not in noun_lst:
+            if splited_input[2] != "COUNTRY":
                 return False
             else:
                 # length 3, 4, 5
                 if len(splited_input) == 3:
-                    if splited_input[2] in noun_lst:
+                    if splited_input[2] == "COUNTRY":
                         return True
                     else:
                         return False
+                # 2 most happy country 2015
                 elif len(splited_input) == 4:
-                    if splited_input[3] not in noun_lst:
-                        if splited_input[3] not in year_lst:
-                            return False
+                    if splited_input[3] != "COUNTRY":
+                        if splited_input[3] in year_lst:
+                            return True
                         else:
-                            return True
-                    elif (splited_input[3] in noun_lst) and (splited_input[3]!=splited_input[2]):
-                            return True
+                            return False
                     else:
                         return False
+                # 2 most happy country in region
+                # 2 most happy country language speaking
                 elif len(splited_input) == 5:
-                    if splited_input[3] not in noun_lst:
-                        return False
-                    elif splited_input[3] == splited_input[2]:
-                        return False
-                    if splited_input[4] not in year_lst:
+                    if splited_input[3] != "IN" and splited_input[3] != "LANGUAGE":
                         return False
                     else:
-                        return True
+                        if splited_input[3] == "IN":
+                            if splited_input[4] == "REGION":
+                                return True
+                            else:
+                                return False
+                        elif splited_input[3] == "LANGUAGE":
+                            if splited_input[4] == "SPEAKING":
+                                return True
+                            else:
+                                return False
+                # 2 most happy country in region 2015
+                # 2 most happy country language speaking 2015
+                elif len(splited_input) == 6:
+                    if splited_input[3] != "IN" and splited_input[3] != "LANGUAGE":
+                        return False
+                    else:
+                        if splited_input[3] == "IN":
+                            if splited_input[4] == "REGION":
+                                if splited_input[5] in year_lst:
+                                    return True
+                                else:
+                                    return False
+                            else:
+                                return False
+                        elif splited_input[3] == "LANGUAGE":
+                            if splited_input[4] == "SPEAKING":
+                                if splited_input[5] in year_lst:
+                                    return True
+                                else:
+                                    return False
+                            else:
+                                return False
 
     else:
         return False
